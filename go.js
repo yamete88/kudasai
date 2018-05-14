5cfcde129e983408c628447e08010fbf010749d8
yamete88 <36991127+yamete88@users.noreply.github.com>
2018-05-14 08:34:22 +0700

    Update go.js

--- go.js
+++ go.js
@@ -132,7 +132,7 @@ function auto_safeconvert() {
         }
         if (a_to_vh == false) {
             var encryptedUrl = Base64.encode(a_to_vi);
-            a_to_ve[i].href = "http://minivert.blogspot.com/p/go.html?url=" + encryptedUrl;
+            a_to_ve[i].href = "https://minivert.blogspot.com/p/go.html?url=" + encryptedUrl;
             a_to_ve[i].rel = "nofollow";
             a_to_vb++;
             a_to_vc += i + ":::" + a_to_ve[i].href + "\n";
